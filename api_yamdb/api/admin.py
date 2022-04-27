a4d0a6a90ec1f1982c08d07730382be9b01f3eb2
dvkonstantinov <121demon@mail.ru>
2022-04-27 22:03:14 +0300

    Созданы модели, сериалайзеры, вьюсеты, роутеры. Настроена проверка значений там где она нужна

--- api_yamdb/api/admin.py
+++ api_yamdb/api/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Genre, Title, Category, TitleGenre
+
+
+class TitleGenreInline(admin.TabularInline):
+    model = TitleGenre
+    extra = 2
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'year', 'category']
+    inlines = (TitleGenreInline,)
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Category, CategoryAdmin)
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,31 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from rest_framework import viewsets
+from rest_framework import viewsets, filters, mixins, status
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .models import  Auth, User, Category, Genre, Title, Review
-from .serializers import  AuthSerializer, UserSerializer, CategorySerializer
+from .permissions import IsAdminOrReadOnlyPermission
+from .models import Auth, User, Category, Genre, Title, Review
+from .serializers import AuthSerializer, UserSerializer, CategorySerializer, \
+    CategoryCreateUpdateSerializer, GenreCreateUpdateSerializer
 from .serializers import GenreSerializer, TitleSerializer, ReviewSerializer
 
 
 class AuthViewSet(viewsets.ModelViewSet):
     pass
 
+
 # Так же не знаю по поводу юзера
 class UserViewSet(viewsets.ModelViewSet):
     pass
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
-    pass
+class CategoryViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin,
+                      mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = Category.objects.all()
+    # permission_classes = (IsAdminOrReadOnlyPermission,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'slug',)
+    lookup_field = 'slug'
+
+    def get_serializer_class(self):
+        if hasattr(self, 'action') and (self.action == 'create'
+                                        or self.action == 'update'):
+            return CategoryCreateUpdateSerializer
+        return CategorySerializer
 
 
-class GenreViewSet(viewsets.ModelViewSet):
-    pass
+class GenreViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin,
+                   mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = Genre.objects.all()
+    # permission_classes = (IsAdminOrReadOnlyPermission,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'slug',)
+    lookup_field = 'slug'
+
+    def get_serializer_class(self):
+        if hasattr(self, 'action') and (self.action == 'create'
+                                        or self.action == 'update'):
+            return GenreCreateUpdateSerializer
+        return GenreSerializer
 
 
 class TitleViewSet(viewsets.ModelViewSet):
-    pass
+    serializer_class = TitleSerializer
+    # permission_classes = (IsAdminOrReadOnlyPermission,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('category__slug', 'genre__slug', 'name', 'year')
+
+    def get_queryset(self):
+        all_fields = {
+            'genre__slug': self.request.query_params.get('genre'),
+            'category__slug': self.request.query_params.get('category'),
+            'name': self.request.query_params.get('name'),
+            'year': self.request.query_params.get('year'),
+        }
+        filter_fields = {k: v for k, v in all_fields.items() if v is not None}
+        queryset = Title.objects.filter(**filter_fields)
+        return queryset
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
