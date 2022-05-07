b3a7e87a5bc8ad3b37ef25320326ad8f5c41ee1f
dvkonstantinov <121demon@mail.ru>
2022-05-07 19:23:37 +0300

    После второй проверки ревьюера

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -8,11 +8,11 @@ class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
 
-    def __str__(self):
-        return self.name
-
     class Meta:
         ordering = ['-id']
+
+    def __str__(self):
+        return self.name
 
 
 class Title(models.Model):
